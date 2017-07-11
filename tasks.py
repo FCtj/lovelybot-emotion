ea9cdf27b0e7eadd212a88e8fb586f6bba622580
FCtj <loveisthegreatest@naver.com>
2017-07-11 11:43:55 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -18,45 +18,47 @@ def echo_response(message):
   print(message)
   
   if message["type"] == "message":
-    if "bitcoin" in message.get("text"):
+    if message.get('text'):
+      if "bitcoin" in message.get("text"):
 
-      r = requests.get("https://api.korbit.co.kr/v1/ticker")
-      bitcoin_price = r.json()["last"]
-      msg = "bitcoin price is %s" % bitcoin_price
-      print(msg)
-      ReplyToActivity(fill=message,
-                    text=msg).send()
+        r = requests.get("https://api.korbit.co.kr/v1/ticker")
+        bitcoin_price = r.json()["last"]
+        msg = "bitcoin price is %s" % bitcoin_price
+        print(msg)
+        ReplyToActivity(fill=message,
+                      text=msg).send()
+      
+      else:
+        data =  {
+          "documents": [
+            {
+              "language": "en",
+              "id": "1",
+              "text": message["text"]
+            }
+          ]
+        }
+        headers = {'Ocp-Apim-Subscription-Key': '4cfe6f744f1b486db3fa83d874bafdd9',
+                   'Content-Type': 'application/json',
+                   'Accept': 'application/json',
+                   }
+
+        r = requests.post("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
+                          data=json.dumps(data),
+                          headers=headers)
+        emo_score = r.json()["documents"][0]["score"]
+        msg = "emotion score is %s\n" % emo_score
+
+        if emo_score > 0.5:
+          msg = msg + "You look happy!"
+        else:
+          msg = msg + "You look unhappy.."
+
+        print(msg)
+
+
+        ReplyToActivity(fill=message,
+                        text=msg).send()
     elif message.get('attachments'):
       ReplyToActivity(fill=message,
-                      text=str(message)).send()
-    else:
-      data =  {
-        "documents": [
-          {
-            "language": "en",
-            "id": "1",
-            "text": message["text"]
-          }
-        ]
-      }
-      headers = {'Ocp-Apim-Subscription-Key': '4cfe6f744f1b486db3fa83d874bafdd9',
-                 'Content-Type': 'application/json',
-                 'Accept': 'application/json',
-                 }
-
-      r = requests.post("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
-                        data=json.dumps(data),
-                        headers=headers)
-      emo_score = r.json()["documents"][0]["score"]
-      msg = "emotion score is %s\n" % emo_score
-
-      if emo_score > 0.5:
-        msg = msg + "You look happy!"
-      else:
-        msg = msg + "You look unhappy.."
-
-      print(msg)
-
-
-      ReplyToActivity(fill=message,
-                      text=msg).send()
+                        text=str(message)).send()
